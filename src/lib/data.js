2445bdbfac0a86774bea2c25405f046d6400754c
rabbitkg <rabbitkgbd@gmail.com>
2026-05-03 06:57:47 +0600

    fixed error.

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,35 +1,19 @@
+import animals from "../../public/animel.json";
+import featuredAnimals from "../../public/featuredAnimals.json";
+import topBreads from "../../public/topBreads.json";
+
 export async function getAnimals() {
-    const res = await fetch(
-        "http://localhost:3000/animel.json",
-    );
-    const data = await res.json();
-    return data;
+    return animals;
 }
 
 export async function getAnimalsDetailsId(id) {
-    const res = await fetch(`http://localhost:3000/animel.json`, {
-        cache: "no-store"
-    });
-    const data = await res.json();
-
-    return data.find(item => item.id == Number(id));
+    return animals.find(item => item.id == Number(id));
 }
-
-
-
-
 
 export async function getFeaturedAnimals() {
-    const res = await fetch(
-        "http://localhost:3000/featuredAnimals.json",
-    );
-    const data = await res.json();
-    return data;
+    return featuredAnimals;
 }
+
 export async function getTopBreads() {
-    const res = await fetch(
-        "http://localhost:3000/topBreads.json",
-    );
-    const data = await res.json();
-    return data;
+    return topBreads;
 }
