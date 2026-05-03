c27c9a0cdb6c0e5bda5d200b55946e51dd5cbda9
rabbitkg <rabbitkgbd@gmail.com>
2026-05-03 06:23:07 +0600

    try to fix error for deployment.

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,8 +1,6 @@
 export async function getAnimals() {
     const res = await fetch(
-        "http://localhost:3000/animel.json",{
-            cache: "no-store"
-        }
+        "http://localhost:3000/animel.json",
     );
     const data = await res.json();
     return data;
